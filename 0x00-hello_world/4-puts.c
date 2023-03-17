3ad5f76dc8c15a81e2682cf36e0255470ff1169d
salahelec2 <salahelec2@gmail.com>
2023-03-16 17:36:45 -0700

    using Puts command in c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * puts function to print message in screen
  *
  * main - is the main function of this code
  *
- * return - always 0 
+ * return - always 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0); 
+	return (0);
 }
