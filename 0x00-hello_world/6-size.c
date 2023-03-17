0d3e4441fc6b9d11ba7a66fad7f539392e5e66fb
salahelec2 <salahelec2@gmail.com>
2023-03-16 18:01:45 -0700

    script to print data size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - the entry and the only function
+ *
+ * Description: 'This code print size of different data type in c'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        printf("Size of a char: %d byte(s)\n",sizeof(char));
+        printf("Size of an int: %d byte(s)\n",sizeof(int));
+        printf("Size of a long int: %d byte(s)\n",sizeof(long));
+        printf("Size of a long long int: %d byte(s)\n",sizeof(unsigned long));
+        printf("Size of a float: %d byte(s)\n",sizeof(int));
+        return (0); /* Return 0 by main function */
+}
