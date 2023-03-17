b3af058226b02aac9f79cf2174a820e8c036d3ca
salahelec2 <salahelec2@gmail.com>
2023-03-16 18:25:09 -0700

    print to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - the entry and the only function
+ *
+ * Description: 'This code print message to the standard error stream'
+ *
+ * Return : 1
+ */
+int main(void)
+{
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1); /* Return 1 by main function */
+}
