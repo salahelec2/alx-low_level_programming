09e965d92cfdeacef8c5c2f3ade3970752f7b3e2
salahelec2 <salahelec2@gmail.com>
2023-03-16 17:51:00 -0700

    using printf command in c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - the entry and the only function
+ *
+ * Description: 'This code print message to the screen followed by new line '
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0); /* Return 0 by main function */
+}
