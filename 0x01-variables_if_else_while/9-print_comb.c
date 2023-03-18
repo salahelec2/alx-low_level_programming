3e98a83c9196b6b4905483da9f419cd364575786
salahelec2 <salahelec2@gmail.com>
2023-03-17 19:13:40 -0700

    print 0 to 9 separated by comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - the entry and the only function
+ *
+ * Description: 'print from 0 to 9 separated by comma'
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphabet[] = "0123456789abcdef";
+	int i;
+
+	for (i = 0; i <= 9 ; ++i)
+	{	
+		putchar(i + 48);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
