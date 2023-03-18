9bfd6ca43321543fea6197ae8cd3f6d291f0fae2
salahelec2 <salahelec2@gmail.com>
2023-03-17 19:04:27 -0700

    base 16 print code

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - the entry and the only function
+ *
+ * Description: 'print from 0 to fi
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphabet[] = "0123456789abcdef"; 
+	int i;
+
+	for (i = 0; i <= 15 ; ++i)
+		putchar([i]);
+	putchar('\n');
+	return (0);
+}
