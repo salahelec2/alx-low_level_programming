0b6abdf74b433297d64f7ffa095d42287fa234ce
salahelec2 <salahelec2@gmail.com>
2023-03-17 18:37:00 -0700

    print digit from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - the entry and the only function
+ *
+ * Description: 'print from 0 to 9'
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9 ; ++i)
+		putchar("%d\n", i);
+	return (0);
+}
