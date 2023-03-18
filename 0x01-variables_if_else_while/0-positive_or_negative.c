8a880a70bad7a603ec8c1f2912009850219fe445
salahelec2 <salahelec2@gmail.com>
2023-03-17 17:28:08 -0700

    repair the positive or negative programme

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,15 +4,13 @@
 /**
  * main - the entry and the only function
  *
- * Description: 'This code test a random number if it is 0 negative or positive'
+ * Description: 'test a random number if it is 0 negative or positive'
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
-
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
