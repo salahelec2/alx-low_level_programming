c0b267b318db59d0f64f0c3860e81e9e7394f0b5
salahelec2 <salahelec2@gmail.com>
2023-03-17 18:09:55 -0700

    print char from a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - the entry and the only function
+ *
+ * Description: 'print from a to z'
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphabet[]="abcdefghijklmnopqrstuvwxyz";
+
+	for (int i = 0; i <= 25 ; ++i)
+		putchar(alphabet[i]);
+	return (0);
+}
