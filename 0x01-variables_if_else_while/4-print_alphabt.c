e065c2a9fce77118d1da6a69b261f39a7dbcfb66
salahelec2 <salahelec2@gmail.com>
2023-03-17 18:32:36 -0700

    print char from a to z in lower and UPPER

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - the entry and the only function
+ *
+ * Description: 'print from a to z'
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphabet[] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; i <= 23 ; ++i)
+		putchar(alphabet[i]);
+	putchar('\n');
+	return (0);
+}
