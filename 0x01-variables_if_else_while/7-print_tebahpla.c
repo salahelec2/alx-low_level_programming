a44ba635d0c1acfdb672c63f3d808fbf3f2dfff4
salahelec2 <salahelec2@gmail.com>
2023-03-17 18:48:43 -0700

    print digit from 0 to 9 followed by new line

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - the entry and the only function
+ *
+ * Description: 'print from z to a'
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 25; i == 0 ; --i)
+		putchar(alphabet[i]);
+	putchar('\n');
+	return (0);
+}
