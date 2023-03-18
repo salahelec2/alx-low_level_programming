f87ae357aea65771fce3db51569ce2b5af203006
salahelec2 <salahelec2@gmail.com>
2023-03-17 18:22:53 -0700

    print char from a to z in lower and UPPER

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,7 +11,7 @@
  */
 int main(void)
 {
-	char alphabetL[] = "abcdefghijklmnopqrstuvwxyz", 
+	char alphabetL[] = "abcdefghijklmnopqrstuvwxyz"; 
 	char alphabetU[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
