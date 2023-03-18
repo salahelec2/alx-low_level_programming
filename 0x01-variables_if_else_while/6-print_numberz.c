9b411fa65546d95b820c447b53bbad815d85b7cc
salahelec2 <salahelec2@gmail.com>
2023-03-17 18:41:44 -0700

    print digit from 0 to 9 followed by new line

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - the entry and the only function
+ *
+ * Description: 'print from 0 to 9'
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9 ; ++i)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+	return (0);
+}
