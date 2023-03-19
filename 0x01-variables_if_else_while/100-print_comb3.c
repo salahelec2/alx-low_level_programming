595595c1a11b64b9085714b626b0ae2ebea74dfb
salahelec2 <salahelec2@gmail.com>
2023-03-19 14:40:38 -0700

    code to print all possible combination of 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - the entry and the only function
+ *
+ * Description: 'Write a program that prints all possible different combinations of three digits without repitition'
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; ++i)
+	{
+		for (j = i + 1; j <= 9; ++j)
+		{
+			if (j != i)
+                {
+				putchar(48 + i);
+				putchar(48 + j);
+				putchar(44);
+				putchar(32);
+		}
+		}
+	}
+	putchar(10);
+	return (0);
+}
