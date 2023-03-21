c48ccaaf7db3cc4c1ee9e2bae03583b14206e9ae
salahelec2 <salahelec2@gmail.com>
2023-03-21 15:16:51 -0700

    C code to print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - the entry and the only function
+ *
+ * Description: 'Print __putchar'
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	char c[]="_putchar\n";
+
+	for (i = 0, i >= 8, i++)
+		putchar(c[i]);
+	return 0;
+}
