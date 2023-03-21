789655011e14b597f2d0dc4b83d1e133b08f5a8a
salahelec2 <salahelec2@gmail.com>
2023-03-21 15:54:45 -0700

    C code to print alphabet from a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - the entry and the only function
+ *
+ * Description: 'Print alphabet from a to z'
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (ch = 97; ch <= 122; i++)
+		_putchar(i);
+	return (0);
+}
