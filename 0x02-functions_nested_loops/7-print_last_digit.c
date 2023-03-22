c058335b5a02eaf73388c4af7935791220e3a6e9
salahelec2 <salahelec2@gmail.com>
2023-03-21 18:55:59 -0700

    C code to return last digit and print it

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ * @n: The number to check
+ *
+ * Return: 0
+ */
+int print_last_digit(int n)
+{
+	int last;
+	last = n % 10;
+	_putchar(last + 48);
+	return (last);
+}	
