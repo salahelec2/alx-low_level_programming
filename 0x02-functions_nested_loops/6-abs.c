9ed0670523d6f61553d8485412efc576b841bd78
salahelec2 <salahelec2@gmail.com>
2023-03-21 18:29:25 -0700

    C code to return abs value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#ifdef _putchar
+#define _putchar
+#include "_putchar.c"
+#endif
+
+/**
+ * _abs - return abolute value
+ * @n: The number to check
+ *
+ * Return: absolute of n
+ */
+int _abs(int n)
+{
+	return ((n*n)/n);
+}
