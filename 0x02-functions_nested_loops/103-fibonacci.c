5f2d5a0e4fef976c9009f3243349d88323a42408
salahelec2 <salahelec2@gmail.com>
2023-03-22 14:27:37 -0700

    Sum of even numbers

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,32 +1,25 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - the only function in this code
- * Description: sum of even numbers.
- * Return: 0
- */
+*main-theonlyfunctioninthiscode
+*Description:sumofevennumbers.
+*Return:0
+*/
 int main(void)
 {
-    long int total, a, b, c;
-
-    total = c = 0;
-    b = 2;
-    a = 1;
-    while (c < 4000000)
-        {
-            c = b + a;
-            a = b;
-            b = c;
-            if ((a % 2) == 0)
-                {
-                    if (total + a > 4000000)
-                        break;
-                    else
-                        total += a;
-                }
-        }
-    printf("%ld", total);
-    return (0);
-
+	long int total, a, b, c;
+	
+	total = c = 0;
+	b = 2;
+	a = 1;
+	while (c < 4000000)
+	{
+		c = b + a;
+		a = b;
+		b = c;
+		if ((a % 2) == 0)
+			total+=a;
+	}
+	printf("%ld",total);
+	return(0);
 }
-
