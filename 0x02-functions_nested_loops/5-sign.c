7ba800bafd24764e4ddeefb24b3b1d3c618c42a3
salahelec2 <salahelec2@gmail.com>
2023-03-21 17:53:25 -0700

    C code to test sign of number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include "_putchar.c"
 /**
  * print_sign - test the signe of given number
  * @n: The number to check
@@ -10,16 +10,16 @@ int print_sign(int n)
 {
 	switch ((n > 0) ? 1 : (n < 0) ? -1 : 0) {
 		case 1:
-			return (1);
 			_putchar(43);
+			return (1);
 			break;
 		case -1:
-			return (-1);
 			_putchar(45);
+			return (-1);
 			break;
 		case 0:
-			return (0);
 			_putchar(48);
+			return (0);
 			break;
 	}
 }
