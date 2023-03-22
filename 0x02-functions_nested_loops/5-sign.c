83dfab72949d96464c0414a23095214fc03bb3c1
salahelec2 <salahelec2@gmail.com>
2023-03-21 18:16:03 -0700

    C code to test sign of number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,16 +12,19 @@
  */
 int print_sign(int n)
 {
-	switch ((n > 0) ? 1 : (n < 0) ? -1 : 0)
+	if (n > 0)
 	{
-		case 1:
-			_putchar(43);
-			return (1);
-		case -1:
-			_putchar(45);
-			return (-1);
-		case 0:
-			_putchar(48);
-			return (0);
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else 
+	{
+		_putchar(48);
+		return (0);
 	}
 }
