0906f039c58317ba9d48bdc85cc1a2399e137dd4
salahelec2 <salahelec2@gmail.com>
2023-03-23 11:19:01 -0700

    print triangle using #

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_triangle - entry point
+ *
+ * Description: Prints triangle using #
+ * @size: size of the triangle
+ * Return: void
+ */
+void print_triangle(int size);
+{
+	int empty, hash, row;
+
+	if (size <= 0)
+	{
+		_putchar(32);
+		return;
+	}
+	for (row = 1; row <= size; row++)
+	{
+		empty = size - row;
+		for (hash = 1; hash <= size; hash++)
+		{
+			if (empty > 0)
+				_putchar(32);
+			else
+				_putchar(35);
+			empty--;
+				
+		}
+	}
+}
+
