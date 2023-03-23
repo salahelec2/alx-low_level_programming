ca4ef33bc295c083ed2a7769ad8aebe794ca717b
salahelec2 <salahelec2@gmail.com>
2023-03-23 10:12:50 -0700

    task 5 print numbers 10 times using _putchar

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - entry point
+ *
+ * Description: Prints the numbers with _putchar
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int n, time;
+
+	for (time = 0; time < 10; time++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n <= 9)
+				_putchar(n + 48);
+			else if (n > 9)
+			{
+				_putchar((n / 10) + 48);
+				_putchar((n % 10) + 48);
+			}
+		}
+	}
+}
