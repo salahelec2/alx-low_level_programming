94c2b96bf532df578f5e3b8476b64d72b3a1f6d0
salahelec2 <salahelec2@gmail.com>
2023-03-23 17:57:29 -0700

    print integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_number - check the code
+ * @n: integer to print
+ *
+ * Return: Always 0.
+ */
+
+void print_number(int n)
+{
+	int digit = 0, temp, i;
+	int devider;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar(45);
+	}
+	temp = n;
+	do {
+		temp /= 10;
+		digit++;
+	} while (temp != 0);
+	for (i = digit; i - 1 > 0; i--)
+	{
+		devider = 10;
+		temp = i - 1;
+		while (temp > 1)
+		{
+			devider *= 10;
+			temp--;
+		}
+		_putchar((n / devider) % 10 + 48);
+		}
+	_putchar(n % 10 + 48);
+}
