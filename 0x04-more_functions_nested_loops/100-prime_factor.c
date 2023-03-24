cc8fe1a730d755e6e641e83376ed40896c667ce6
salahelec2 <salahelec2@gmail.com>
2023-03-23 19:27:55 -0700

    factor code

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
-int is_prime(long long int n);
+int is_prime(unsigned long int n);
 int main(void)
 {
-long long int n = 612852475143;
+       unsigned	long int n = 612852475143, i;
 
-for (long long int i = 2; i <= sqrt(n); i++)
+for (i = 2; i <= sqrt(n); i++)
 {
 	if (n % i == 0 && is_prime(i))
 	{
-		printf("%lld ", i);
+		printf("%lu ", i);
 		while (n % i == 0)
 		{
 			n /= i;
@@ -18,16 +18,17 @@ for (long long int i = 2; i <= sqrt(n); i++)
 	}
 }
 if (n > 1)
-	printf("%lld", n);
+	printf("%lu", n);
 printf("\n");
 return (0);
 }
 
-int is_prime(long long int n)
+int is_prime(unsigned long int n)
 {
+	unsigned long int i; 
 	if (n <= 1)
 		return (0);
-	for (long long int i = 2; i <= sqrt(n); i++)
+	for (i = 2; i <= sqrt(n); i++)
 	{
 		if (n % i == 0)
 			return (0);
