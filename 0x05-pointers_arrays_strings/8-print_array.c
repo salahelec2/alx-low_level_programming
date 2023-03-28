8128ab24cec2d62c552f95e2e1efd44394f482b1
salahelec2 <salahelec2@gmail.com>
2023-03-28 11:07:54 -0700

    task 8 print specified elements of array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -17,9 +17,9 @@ void print_array(int *a, int n)
 	{
 		if (i == n - 1)
 			break;
-		printf("%d, ",*(a + i));
+		printf("%d, ", *(a + i));
 	}
 	if (n > 0)
-		printf("%d",*(a + i));	
+		printf("%d", *(a + i));
 	putchar(10);
 }
