f33dc047dd62ebfa106e730a8cdc2fab28056a26
salahelec2 <salahelec2@gmail.com>
2023-03-28 10:42:40 -0700

    task 7 half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - print half
+ **@str: string
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i, j;
+
+	for (i = 0 ; str[i] != '\0'; i++)
+		;
+	for (j = (i / 2); j < i ; ++j)
+	{
+		_putchar(str[j]);
+	}
+	if ((i - 1) % 2)
+		_putchar(str[j]);
+	_putchar(10);
+}
