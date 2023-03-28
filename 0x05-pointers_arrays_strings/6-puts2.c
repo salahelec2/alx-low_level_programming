d5ea54dd6debf0571415ea9a16bdd6aef470d949
salahelec2 <salahelec2@gmail.com>
2023-03-27 21:06:07 -0700

    Pointers task 7 half string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - print half
+ **@str: string
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i, j;
+
+	for (i = 0;str[i] != '\0'; i++)
+		;
+	for (j = 0 ; j <= i; j++)
+	{
+		if (str[j] == '\0')
+			continue;
+		if (j % 2 == 0)
+			_putchar(str[j+1]);
+	}
+	_putchar(10);
+}
+	
+
