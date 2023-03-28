fb3dea7d7b345820d48e5f09e039d0830f672c8d
salahelec2 <salahelec2@gmail.com>
2023-03-28 11:25:08 -0700

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - print half
+ **@str: string
+ *
+ * Return: void
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = scr[i];
+	}
+	dest[i] = src[i];
+	return (dest);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,3 +12,4 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
