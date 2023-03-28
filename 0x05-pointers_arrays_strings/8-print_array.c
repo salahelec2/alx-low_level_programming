e7a1e69203d88ab7aeafd502df918fa27dc604e3
salahelec2 <salahelec2@gmail.com>
2023-03-28 11:00:48 -0700

    task 8 print specified elements of array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,8 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * puts2 - print half
- **@str: string
+ * print_array - print half print specified ele;ents of array
+ **@a: array
+ **@n: number of element to be printed
  *
  * Return: void
  */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+			break;
+		printf("%d, ",*(a + i));
+	}
+	printf("%d",*(a + i));	
+	putchar(10);
+}
