34e5ac488798e71b229cd206558b3a940e4da1b5
salahelec2 <salahelec2@gmail.com>
2023-03-28 18:41:23 -0700

    task 10 resolved

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,29 +7,36 @@
  * Return: integer
  */
 
-
-
-
 int _atoi(char *s)
 {
-	int num = 0;
-	int sign = 1;
-	int i = 0;
+	int num, signP, signN, i, result, sign;
 
+	num = 0;
+	signP = 0;
+	signN = 0;
+	i = 0;
+	result = 0;
+	sign = 1;
 	while (s[i] != '\0')
-	{
+		{
 		if (num > 0 && !(s[i] >= 48 && s[i] <= 57))
 			break;
+		if (s[i] == '-' && num == 0)
+			signN++;
+		if (s[i] == '+' && num == 0)
+			signP++;
 		if (s[i] >= 48 && s[i] <= 57)
-			{
-			if (s[i - 1] == '-' && num == 0)
-				sign = -1;
-			num = num * 10 + (s[i] - '0');
-			i++;
-			}
+		{
+		num = num * 10 + (s[i] - '0');
+		if (signP >= signN)
+			sign = 1;
+		else if (signP < signN)
+			sign = -1;
+		result = num * sign;
+		i++;
+		}
 		else
 			i++;
-	}
-
-	return (num * sign);
+		}
+	return (result);
 }
