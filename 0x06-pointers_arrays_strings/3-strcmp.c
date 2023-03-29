72c89c7e69a6d990844b9050e3e72e6a730f5fae
salahelec2 <salahelec2@gmail.com>
2023-03-29 15:44:48 -0700

    task 3 strcmp created

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - Compares the string s1 to the string  s2.
+ * @s1: the host string
+ * @s2: this string will be copied to the host string
+ * Return: compareson value
+ */
+
+char _strcmp(char *s1, char *s2)
+{
+	char* comp;
+	comp = strcmp(s1, s2);
+	return (comp);
+}
