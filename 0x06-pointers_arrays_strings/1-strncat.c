7210f27ffa07b37a260e3cdaa63474137d76b563
salahelec2 <salahelec2@gmail.com>
2023-03-29 11:25:06 -0700

    task 1 strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - append dest using src string by n char 
+ * @dest: the host string
+ * @src: this string will be apended to the host string
+ * @n: number of character to be appended by
+ * Return: concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+    return (strncat(dest, src, n));
+}
