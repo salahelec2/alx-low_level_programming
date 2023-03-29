09870970ecc8e8425aff519471a6ee2a70b04fcc
salahelec2 <salahelec2@gmail.com>
2023-03-29 16:18:27 -0700

    task 4 reverse string

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array - reverse an array
+ * @a: array
+ * @n: array elements
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j;
+	char temp;
+
+	for (i = n - 1, j = 0; i >= 0, j <= (n - 1) / 2; i--, j++)
+	{
+		temp = a[j];
+		a[j] = a[i];
+		a[i] = temp;
+	}
+}
