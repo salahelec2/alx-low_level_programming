54a7c74c02db6bae1a5f758d7b14523626952e9d
salahelec2 <salahelec2@gmail.com>
2023-04-03 14:15:22 -0700

    task 0 in folder 7

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - Conacatenates two strings together
+ * @s: string to be modified
+ * @b: word filler
+ * @n: number of elements to fill
+ * Return: dest
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i =< n; i++)
+	{
+		s[i] = b;
+	}
+}
