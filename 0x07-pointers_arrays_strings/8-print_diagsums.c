80cabcdad7407339f0279e6b3c5a42906c6f1f27
salahelec2 <salahelec2@gmail.com>
2023-04-03 18:47:23 -0700

    task 8 print diagonal sum solved

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_diagsums - print diag sum of a multi dim array
@@ -9,18 +10,18 @@
 
 void print_diagsums(int *a, int size)
 {
-	unsigned int i, j;
+	int i;
 	unsigned long int sum;
 
 	sum = 0;
 	for (i = 0; i < size ; i++)
 	{
-		sum += a[i][i];
+		sum += a[i * size + i];
 	}
-	printf("%ul, ", sum);
+	printf("%lu, ", sum);
 	for (i = size - 1; i > 0; i--)
 	{
 		sum += a[i * size + i];
 	}
-	printf("%ul\n", sum);
+	printf("%lu\n", sum);
 }
