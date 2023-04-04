c790c75c32a02926e8ea693a93cb1f68bb4dd391
salahelec2 <salahelec2@gmail.com>
2023-04-03 18:05:53 -0700

    task 6 print cheesboard folder 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print elements of multi dimentional array
+ * @a : string to be printed
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i, j;
+
+	for (i = 0; i < 8 ; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			putchar(a[i][j]);
+		}
+		if (i != 7)
+			putchar('\n');
+	}
+}
