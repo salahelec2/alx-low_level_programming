0ad68a7b87c1b8a28e28ab068f3d6e8b3ed81d63
salahelec2 <salahelec2@gmail.com>
2023-04-11 14:55:50 -0700

    function to fill an array with a specific value add an error message when returning 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,6 +16,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int n;
 	char *ptr;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	ptr = (char *) malloc(size * sizeof(char));
 	if (ptr == NULL)
 	{
