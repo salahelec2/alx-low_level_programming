87d3ed22328043bc8fcce5bc00b9aee43b8020b7
salahelec2 <salahelec2@gmail.com>
2023-04-11 15:09:32 -0700

    function to copy string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - return a pointer to a memory filled with given string
+ *
+ * @str: the given string
+ *
+ * Return: pointer to memory or NULL when insufficient memory
+ */
+char *_strdup(char *str)
+{
+	char *str_2;
+	int len, b;
+
+	len = strlen(str);
+	if (str == NULL)
+		return (NULL);
+	str_2 = (char *)  malloc((len + 1) * sizeof(char));
+	if (str_2 == NULL)
+		return (NULL);
+	strcpy(str_2, str);
+	return (str_2);
+}
