86bccf6ae79943b3b2a854ec99fda44cbd36ff7e
salahelec2 <salahelec2@gmail.com>
2023-04-11 15:22:27 -0700

    function to copy string resolved

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,16 +12,21 @@
 char *_strdup(char *str)
 {
 	char *str_2;
-	int len;
+	int len, cp;
 
 	if (str == NULL)
 		return (NULL);
-	len = strlen(str);
+	for (len = 0; str[len] != '\0'; len++)
+		;
 	if (str == NULL)
 		return (NULL);
 	str_2 = (char *)  malloc((len + 1) * sizeof(char));
 	if (str_2 == NULL)
 		return (NULL);
-	strcpy(str_2, str);
+	for (cp = 0; cp <= len; cp++)
+	{
+		str_2[cp] = str[cp];
+	}
+	str_2[len + 1] = '\0';
 	return (str_2);
 }
