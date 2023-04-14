0b7520331c7a8eb763fa45d863e6635d69e45b00
salahelec2 <salahelec2@gmail.com>
2023-04-13 18:22:38 -0700

    concatenate 2 srings return adress of allocated memory for the concatenated string

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - add two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: amount of bytes.
+ *
+ * Return: pointer to the allocated memory, exit status 98 if fails
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concString;
+	unsigned int str1Len, str2Len, i;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[tr1Len] != '\0')
+		str1Len++;
+	while (s2[str2Len] != '\0')
+		str2Len++;
+	if (n >= str2Len)
+		n = str2Len;
+	concString = malloc(str1Len + n + 1);
+	if (concString == NULL)
+		return (NULL);
+	for (i = 0; i < str1Len; i++)
+	{
+		concString[i] = s1[i];
+	}
+	for (i = 0; i < n; i++)
+	{
+		concatenated_string[s1_length + i] = s2[i];
+	}
+	concString[str1Len + n] = '\0';
+	return (concString);
+}
+
