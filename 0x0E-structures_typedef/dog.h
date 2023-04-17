c33b7ad04259b2a2ca3a29193a2ddd98d0920909
salahelec2 <salahelec2@gmail.com>
2023-04-17 15:46:11 -0700

    task0 dog structure definition resolved betty doc

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog {
+/**
+ * struct dog - dog structure definition
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ *
+ * Description: in this struct i will store name and age and owner for a dog
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
