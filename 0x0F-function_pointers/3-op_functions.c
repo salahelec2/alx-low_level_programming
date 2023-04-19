720b001d393ae2767387158ced46c94e29342e91
salahelec2 <salahelec2@gmail.com>
2023-04-19 16:23:14 -0700

    task 3 simple calculator

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add two numbers
+ *
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: sum of 2 numbers
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substruction of 2 numbers
+ *
+ * @a:  number 1
+ * @b:  number 2
+ *
+ * Return: substruction of 2 numbers
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply two numbers
+ *
+ * @a:  number 1
+ * @b:  number 2
+ *
+ * Return: multiplation of two numbers
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_mod - modulo of two numbers
+ *
+ * @a:  number 1
+ * @b:  number 2
+ *
+ * Return: modulo of two numbers
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * op_div - devide two numbers
+ *
+ * @a:  number 1
+ * @b:  number 2
+ *
+ * Return: division of two numbers
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
