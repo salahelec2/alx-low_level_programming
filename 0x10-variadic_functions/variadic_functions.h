c2d5a959ab2ccc6c8745ab47a83061e9dee856e4
salahelec2 <salahelec2@gmail.com>
2023-04-20 16:57:42 -0700

    task 1 print numbers followed by new line

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,7 @@
 #ifndef VARIADIC_FUNCTION_H
 #define VARIADIC_FUNCTION_H
+#include <stdio.h>
+#include <stdlib.h>
 void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
