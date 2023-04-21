dd5df06c1efdb2d32f6e219094a80889ea5c0914
salahelec2 <salahelec2@gmail.com>
2023-04-20 17:04:24 -0700

    task 2 print strings separated with specified separator followed by new line

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_strings -  print strings separated by specified separator 
+ *		followed by new line.
+ *
+ * @separator: wanted separator
+ * @n: number of inputs
+ *
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int c;
+
+	va_start(args, n);
+	for (c = 0; c < n; c++)
+	{
+		char *s;
+
+		s = va_arg(args, char *);
+		if (s == NULL)
+			printf("(nil)");
+		else
+			printf("%s", s);
+		if (separator != NULL && c != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
