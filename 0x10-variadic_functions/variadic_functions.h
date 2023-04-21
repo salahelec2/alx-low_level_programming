80f98a1652ef547bfade875cc8e40e9bd77c94d0
salahelec2 <salahelec2@gmail.com>
2023-04-20 17:05:29 -0700

    task 2 add function prototype to header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,7 @@
 #define VARIADIC_FUNCTION_H
 #include <stdio.h>
 #include <stdlib.h>
+void print_strings(const char *separator, const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
