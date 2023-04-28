9f023137cbfa971489fc09b5e30ac70704d2e1d3
salahelec2 <salahelec2@gmail.com>
2023-04-27 18:02:33 -0700

    task 4 resolved

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,6 +19,7 @@ typedef struct list_s
 } list_t;
 
 char *_strdup(const char *s);
+void free_list(list_t *head);
 size_t print_list(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
