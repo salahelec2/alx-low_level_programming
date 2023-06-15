36732efd167b28b77a9a4ff7d18c027ecf963c43
salahelec2 <salahelec2@gmail.com>
2023-06-15 13:07:08 -0700

    task 4

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -12,7 +12,7 @@ typedef struct dlistint_s
 
 void free_dlistint(dlistint_t *head);
 size_t print_dlistint(const dlistint_t *h);
-
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
